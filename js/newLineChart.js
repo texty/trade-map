505fbaa00d0a5c468eedb04b037af6adb32c9781
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2019-11-27 20:18:02 +0200

    додала привид неуніфікованої шкали і лейби мін. та макс. значень

--- js/newLineChart.js
+++ js/newLineChart.js
@@ -3,6 +3,11 @@
  */
 var parseDate = d3.timeParse("%Y");
 
+var margin = {top: 10, right: 20, bottom: 15, left: 20},
+    width = 300 - margin.left - margin.right,
+    height = 100 - margin.top - margin.bottom,
+    rectWidth = 5;
+
 var chart_data;
 function retrieve_chart_data(cb) {
     if (chart_data) return cb(chart_data);
@@ -24,104 +29,83 @@ function retrieve_chart_data(cb) {
 }
 
 retrieve_chart_data(function(myData){
-    var eu__I = [];
-    var eu__E = [];
-    var ru__I = [];
-    var ru__E = [];
+    var ExpArr_ru = [];
+    var ImpArr_ru = [];
+    var ExpArr_eu = [];
+    var ImpArr_eu = [];
 
     myData.forEach(function(d) {
         if(d.country === "Росія" && d.code != "TOTAL" && d.product != "All products" && d.Exported > 0 && d.Exported > d.Imported ){
-            ru__E.indexOf(d.product) === -1 ? ru__E.push(d.product) : console.log("");
+            ExpArr_ru.push(d);
         } else if (d.country === "Росія" && d.code != "TOTAL" && d.product != "All products" && d.Imported > 0 && d.Imported > d.Exported){
-            ru__I.indexOf(d.product) === -1 ? ru__I.push(d.product) : console.log("");
+            ImpArr_ru.push(d);
         } else if (d.country === "ЄС" && d.code != "TOTAL" && d.product != "All products" && d.Imported > 0 && d.Imported > d.Exported){
-            eu__I.indexOf(d.product) === -1 ? eu__I.push(d.product) : console.log("");
+            ImpArr_eu.push(d)
         } else if (d.country === "ЄС" && d.code != "TOTAL" && d.product != "All products" && d.Exported > 0 && d.Exported > d.Imported){
-            eu__E.indexOf(d.product) === -1 ? eu__E.push(d.product) : console.log("");
+            ExpArr_eu.push(d)
         }
     });
 
-    // var appendLi = function(dataset, container, country, type) {
-    //     d3.select(container)
-    //         .selectAll("li")
-    //         .data(dataset)
-    //         .enter()
-    //         .append("li")
-    //         .style("color", "grey")
-    //         .text(function(d) {
-    //             return d
-    //         })
-    //         .attr("country", country)
-    //         .attr("type", type)
-    //
-    //         .on("click", function (item) {
-    //             $(this).css("text-decoration", "underline");
-    //             var country = $(this).attr("country");
-    //             var type = $(this).attr("type");
-    //             drawLines(myData, item, country, type);
-    //         });
-    // };
+    /* знаходимо максимальні значення для кожного з чотирьох блоків */
+    var maxExp_ru = d3.max(ExpArr_ru, function(d) { return +d.Exported; } );
+    var maxImp_ru = d3.max(ImpArr_ru, function(d) { return +d.Imported; } );
+    var maxExp_eu = d3.max(ExpArr_eu, function(d) { return +d.Exported; } );
+    var maxImp_eu = d3.max(ImpArr_eu, function(d) { return +d.Imported; } );
 
-    //appendLi(eu__I, "#eu-i-items", "ЄС", "Imported");
-    //appendLi(eu__E, "#eu-e-items", "ЄС", "Exported");
-    //appendLi(ru__E, "#ru-e-items", "Росія", "Exported");
-    //appendLi(ru__I, "#ru-i-items", "Росія", "Imported");
 
-    ru__I.forEach(function(product){
-        drawLines(myData, product, "Росія", "Imported");
+    /* малюємо дефолтну Росія */
+    _.uniq(ImpArr_ru, "product").forEach(function(d){
+        drawLines(myData, d.product, "Росія", "Imported", maxImp_ru);
+    });
+
+    _.uniq(ExpArr_ru, "product").forEach(function(d){
+        drawLines(myData, d.product, "Росія", "Exported", maxExp_ru);
     });
 
 
-    ru__E.forEach(function(product){
-        drawLines(myData, product, "Росія", "Exported");
-    });
-
+    /* перемикач між країнами */
     $("#show-eu").on("click", function(d) {
         /* міняємо стиль кнопок */
-        //$("#show-eu").prependTo("#ru-eu");
         d3.select("#show-ru").style("background-color", "white").style("color", "grey");
         d3.select("#show-eu").style("background-color", "grey").style("color", "white");
-        d3.select("#selected-country").html("Торгівля з ЄС, 2002-2018");
+        d3.select("#selected-country").html("Торгівля з ЄС, 2001-2018");
 
         /* малюємо графіки */
         d3.selectAll(".svg-wrapper").remove();
 
-        eu__I.forEach(function(product){
-            drawLines(myData, product, "ЄС", "Imported");
+        _.uniq(ImpArr_eu, "product").forEach(function(d){
+            drawLines(myData, d.product, "ЄС", "Imported", maxImp_eu);
         });
 
-
-        eu__E.forEach(function(product){
-            drawLines(myData, product, "ЄС", "Exported");
+        _.uniq(ExpArr_eu, "product").forEach(function(d){
+            drawLines(myData, d.product, "ЄС", "Exported", maxExp_eu);
         });
 
+        /* якщо вже обраний експорт, то його і показуємо */
         if(showExport === true){
             d3.selectAll(".svg-wrapper.Exported").style("display", "block");
             d3.selectAll(".svg-wrapper.Imported").style("display", "none");
         }
-
-
     });
+
 
     /* Показати Росію */
     $("#show-ru").on("click", function(d) {
-        //$("#show-ru").prependTo("#ru-eu");
         d3.select("#show-eu").style("background-color", "white").style("color", "grey");
         d3.select("#show-ru").style("background-color", "grey").style("color", "white");
-        d3.select("#selected-country").html("Торгівля з Росією, 2002-2018")
-        
+        d3.select("#selected-country").html("Торгівля з Росією, 2001-2018");
         
         d3.selectAll(".svg-wrapper").remove();
-        
-        ru__I.forEach(function(product){
-            drawLines(myData, product, "Росія", "Imported");
+
+        _.uniq(ImpArr_ru, "product").forEach(function(d){
+            drawLines(myData, d.product, "Росія", "Imported", maxImp_ru);
         });
 
-
-        ru__E.forEach(function(product){
-            drawLines(myData, product, "Росія", "Exported");
+        _.uniq(ExpArr_ru, "product").forEach(function(d){
+            drawLines(myData, d.product, "Росія", "Exported", maxExp_ru);
         });
 
+        /* якщо вже обраний експорт, то його і показуємо */
         if(showExport === true){
             d3.selectAll(".svg-wrapper.Exported").style("display", "block");
             d3.selectAll(".svg-wrapper.Imported").style("display", "none");
@@ -131,56 +115,29 @@ retrieve_chart_data(function(myData){
 });
 
 
-var drawLines = function(data, key, country, type) {
 
 
+var drawLines = function(data, key, country, type, maxRange) {
     //d3.select('#line-chart > svg').remove();
 
     var testData =  data
-            .filter(function (d) {
-                return d.country === country && d.product === key;
-            })
-            .sort(function (a, b) {
-                return b.year - a.year
-            });
+        .filter(function (d) {
+            return d.country === country && d.product === key;
+        })
+        .sort(function (a, b) {
+            return b.year - a.year
+        });
 
+    /* максимальне значення саме цього графіку для перемальовки */
+    var maxDataValue = d3.max(testData, function (d) { return Math.max(d.Exported, d.Imported);  });
 
-    var margin = {top: 10, right: 0, bottom: 15, left: 5},
-        width = 250 - margin.left - margin.right,
-        height = 100 - margin.top - margin.bottom,
-        rectWidth = 5;
-
-    //linechartTest
-    var svgContainer = d3.select("#line-chart")
-        .append("div")
-        .attr("class", "svg-wrapper "+ type)
-        .style("width", '250px')
-        .style("height", '200px');
-
-
-          var svg = svgContainer.append("svg")
-    //.attr("width", width + margin.left + margin.right)
-    //.attr("height", height + margin.top + margin.bottom)
-    //     .attr('preserveAspectRatio', "xMinYMin meet")
-    //     .attr("viewBox", "0 0 400 200")
-
-        .attr('width', '250px')
-        .attr('height', '120px')
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-    var svgTitle = svgContainer
-        .append("div")
-        .attr("class", "svg-title")
-        .style("width", '250px')
-        .text(key);
 
     var x = d3.scaleTime().range([0, width]);
     var y = d3.scaleLinear().range([height, 0]);
 
+    var ySelf = d3.scaleLinear().range([height, 0]);
 
-    // define the 1st line
+   // define the 1st line
     var valueline = d3.line()
         .x(function (d) {
             return x(d.year);
@@ -189,72 +146,168 @@ var drawLines = function(data, key, country, type) {
             return y(d[type]);
         });
 
-    // define the 2nd line
-    var valueline2 = d3.line()
+    var selfline = d3.line()
         .x(function (d) {
             return x(d.year);
         })
         .y(function (d) {
-            return y(d[type]);
+            return ySelf(d[type]);
         });
 
+    //linechartTest
+    var svgContainer = d3.select("#line-chart")
+        .append("div")
+        .attr("class", "svg-wrapper "+ type)
+        .style("width", '300px')
+        .style("height", '200px')
+        .on("mouseover", function(d) {
+            $('.ghost').css("display", "none");
+            $(this).find("svg").find('.ghost').css("display", "block")
+        })
+        .on("mouseleave", function(d) {
+            $('.ghost').css("display", "none");
+        });
+
+
+    var svg = svgContainer.append("svg")
+        .attr('width', '300px')
+        .attr('height', '120px')
+        .attr("data", maxDataValue)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    svgContainer
+        .append("div")
+        .attr("class", "svg-title")
+        .style("width", '300px')
+        .text(key);
+
+
     x.domain([new Date(2001, 0, 1), new Date(2018, 11, 31)]);
-    //x.domain(d3.extent(testData, function (d) {  return d.year; }));
-    y.domain([0, d3.max(testData, function (d) {
-        return Math.max(d.Exported, d.Imported);
-    })]);
+    ySelf.domain([0, d3.max(testData, function (d) { return Math.max(d.Exported, d.Imported);  })]);
+    y.domain([0, maxRange]);
 
+    var ghost = svg.append("g").attr("class", "ghost");
 
-    // Add the valueline path.
+    // Єдина шкала
     svg.append("path")
         .data([testData])
         .attr("class", "valueline1")
         .style("stroke", type === "Exported" ? exColor : imColor)
         .attr("d", valueline);
 
-    // // Add the valueline2 path.
-    // svg.append("path")
-    //     .data([testData])
-    //     .attr("class", "valueline2")
-    //     .style("stroke", imColor)
-    //     .attr("d", valueline2);
+    //Персональна шкала
+    ghost.append("path")
+        .data([testData])
+        .attr("class", "self-line")
+        .style("stroke", "grey")
+        .style("stroke-dasharray", ("3, 3"))
+        .style("fill", "none")
+        .attr("d", selfline);
 
-    // svg.selectAll("line-circle")
-    //     .data(testData)
-    //     .enter().append("rect")
-    //     .attr("class", "data-circle")
-    //     .attr("width", rectWidth)
-    //     .attr("height", rectWidth)
-    //     .style("margin-left", -2.5)
-    //     .attr("x", function(d) { return x(d.year)  - rectWidth/2; })
-    //     .attr("y", function(d) { return y(d[type]) - rectWidth/2; })
-    //     .attr("fill", type === "Exported" ? exColor : imColor);
+
+    ghost
+        .selectAll('text')
+        .data(testData)
+        .enter()
+        .append('text')
+        .filter(function(d, i) { return i === 0 || i === (testData.length - 1) })
+        .classed('label', true)
+        .attr('x', function(d) {
+                return x(d.year) - 15;
+            })
+        .attr("y", function(d) {
+                return ySelf(d[type]) - 10;
+
+        })
+        .style("font-size", '10px')
+        .style("fill", 'grey')
+        .text(function(d){
+            return Math.floor(d[type]/1000);
+        })
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // ghost.append("text")
+    //     .attr("x", 0)
+    //     .attr("y", 0)
+    //     .text("млн $")
+    //     .style("font-size", '10px')
+    //     .style("fill", 'grey');
+
 
     // Add the X Axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
-
-    // Add the Y Axis
-    // svg.append("g")
-    //     .call(d3.axisLeft(y).ticks(5));
-
-
-    // //labels
-    // svg.append("text")
-    //     .attr("transform", "translate(" + (width + 3) + "," + y(testData[0].Imported) + ")")
-    //     .attr("dy", ".35em")
-    //     .attr("text-anchor", "start")
-    //     .style("fill", imColor)
-    //     .text("Імпорт");
-    //
-    // svg.append("text")
-    //     .attr("transform", "translate(" + (width + 3) + "," + y(testData[0].Exported) + ")")
-    //     .attr("dy", ".35em")
-    //     .attr("text-anchor", "start")
-    //     .style("fill", "#c01788")
-    //     .text("Експорт");
-
-    d3.select("#linechart-title").text(key);
-
 };
+
+
+// var reDraw = function() {
+//     $("svg").each(function(i){
+//         var currentChart = $("svg")[i];
+//         var currentChartDomain = $(currentChart).attr("data");
+//
+//         var y = d3.scaleLinear()
+//             .range([height, 0])
+//             .domain([0, currentChartDomain]);
+//
+//         /* Лінія експорту */
+//         var exportLine = d3.line()
+//             .x(function (d) {  return x(d.year); })
+//             .y(function (d) {  return y(d.Exported);  });
+//
+//
+//         /* Лінія імпорту */
+//         var importLine = d3.line()
+//             .x(function (d) { return x(d.year);  })
+//             .y(function (d) { return y(d.Imported); });
+//
+//
+//         var currentLine = $(currentChart.childNodes[0])
+//             .find("path");
+//
+//         if(showExport === true){
+//
+//             d3.select(currentLine[0])
+//                 .transition()
+//                 .duration(750)
+//                 .attr("d", exportLine);
+//
+//         } else if (showImport === true) {
+//             d3.select(currentLine[0])
+//                 .transition()
+//                 .duration(750)
+//                 .attr("d", importLine);
+//         }
+//
+//     });
+//
+// };
+//
+// $("#redraw").on("click", reDraw);
+
+
+
+
+
+
+
+
+// // Add the valueline2 path.
+// svg.append("path")
+//     .data([testData])
+//     .attr("class", "valueline2")
+//     .style("stroke", imColor)
+//     .attr("d", valueline2);
+
+// svg.selectAll("line-circle")
+//     .data(testData)
+//     .enter().append("rect")
+//     .attr("class", "data-circle")
+//     .attr("width", rectWidth)
+//     .attr("height", rectWidth)
+//     .style("margin-left", -2.5)
+//     .attr("x", function(d) { return x(d.year)  - rectWidth/2; })
+//     .attr("y", function(d) { return y(d[type]) - rectWidth/2; })
+//     .attr("fill", type === "Exported" ? exColor : imColor);
