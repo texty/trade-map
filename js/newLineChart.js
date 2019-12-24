01bf27eb7f48ce248c2a46708c3bd908b916ee87
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2019-12-24 15:08:39 +0200

    графічки різного розміру

--- js/newLineChart.js
+++ js/newLineChart.js
@@ -3,7 +3,7 @@
  */
 var margin = {top: 10, right: 20, bottom: 15, left: 20},
     width = 300 - margin.left - margin.right,
-    height = 100 - margin.top - margin.bottom;
+    height = 300 - margin.top - margin.bottom;
 
 retrieve_chart_data(function(myData){
     var ExpArr_ru = [];
@@ -79,10 +79,22 @@ retrieve_chart_data(function(myData){
     /* малюємо дефолтнe ЗАГАЛОМ */
     _.uniq(ImpArr_all, "product").forEach(function(d){
         drawMultiples(myData, d.product, "загалом", "Imported", maxImp_all, "#line-chart");
+        // $(".svg-wrapper.Exported").appendTo(".exported-top");
+        // $(".svg-wrapper.Imported").appendTo(".imported-top");
+
+
+        $('#line-chart').find('.svg-wrapper.Imported').sort(function (a, b) {
+            return $(b).attr('data') - $(a).attr('data');
+        }).appendTo('#line-chart');
+
     });
 
     _.uniq(ExpArr_all, "product").forEach(function(d){
         drawMultiples(myData, d.product, "загалом", "Exported", maxExp_all, "#line-chart");
+
+        $('#line-chart').find('.svg-wrapper.Exported').sort(function (a, b) {
+            return $(b).attr('data') - $(a).attr('data');
+        }).appendTo('#line-chart');
     });
 
 
@@ -91,9 +103,17 @@ retrieve_chart_data(function(myData){
         drawMultiples(myData, d.product, "cases", "Imported", maxImp_cases, "#cases-chart");
     });
 
+    $('#cases-chart').find('.svg-wrapper.Imported').sort(function (a, b) {
+        return $(b).attr('data') - $(a).attr('data');
+    }).appendTo('#cases-chart');
+
     _.uniq(ExpArr_cases, "product").forEach(function(d){
         drawMultiples(myData, d.product, "cases", "Exported", maxExp_cases, "#cases-chart");
     });
+
+    $('#cases-chart').find('.svg-wrapper.Exported').sort(function (a, b) {
+        return $(b).attr('data') - $(a).attr('data');
+    }).appendTo('#cases-chart');
 
 
 
@@ -104,6 +124,8 @@ retrieve_chart_data(function(myData){
         var targetArray = correspondings.filter(function(d){ return d.country === selectedCountry });
 
         d3.selectAll("#line-chart >.svg-wrapper").remove();
+        d3.selectAll(".exported-top >.svg-wrapper").remove();
+        d3.selectAll(".imported-top >.svg-wrapper").remove();
 
         targetArray.forEach(function(d){
             _.uniq(d.data, "product").forEach(function(k){
@@ -111,6 +133,14 @@ retrieve_chart_data(function(myData){
             });
 
         });
+
+        $('#line-chart').find('.svg-wrapper.Imported').sort(function (a, b) {
+            return $(b).attr('data') - $(a).attr('data');
+        }).appendTo('#line-chart');
+        $('#line-chart').find('.svg-wrapper.Exported').sort(function (a, b) {
+            return $(b).attr('data') - $(a).attr('data');
+        }).appendTo('#line-chart');
+
 
         d3.select("#selected-country").html("Торгівля " + targetArray[1].text +", 2001-2018");
 
@@ -132,6 +162,13 @@ retrieve_chart_data(function(myData){
             });
 
         });
+
+        $('#line-chart').find('.svg-wrapper.Imported').sort(function (a, b) {
+            return $(b).attr('data') - $(a).attr('data');
+        }).appendTo('#line-chart');
+        $('#line-chart').find('.svg-wrapper.Exported').sort(function (a, b) {
+            return $(b).attr('data') - $(a).attr('data');
+        }).appendTo('#line-chart');
 
         d3.select("#selected-country").html("Торгівля" + targetArray[1].text +", 2001-2018");
 
@@ -160,8 +197,10 @@ var drawMultiples = function(data, key, country, type, maxRange, container) {
             return b.year - a.year
         });
 
+    console.log(testData);
+
     /* максимальне значення саме цього графіку для перемальовки */
-    var maxDataValue = d3.max(testData, function (d) { return Math.max(d.Exported, d.Imported);  });
+    var maxDataValue = d3.max(testData, function (d) { return Math.max(d[type]);  });
 
 
     var x = d3.scaleTime().range([0, width]);
@@ -189,24 +228,38 @@ var drawMultiples = function(data, key, country, type, maxRange, container) {
     //linechartTest
     var svgContainer = d3.select(container)
         .append("div")
-        .attr("class", "svg-wrapper "+ type)
+        .attr("class", "svg-wrapper " + type)
         .style("width", '300px')
-        .style("height", '200px');
+        .style("height", 'max-content')
+        .attr("data", maxDataValue);
 
 
     var svg = svgContainer.append("svg")
         .attr('width', '300px')
-        .attr('height', '120px')
+        .attr('height', 50 + (maxDataValue/(maxRange/300)) + "px")
         .attr("data", maxDataValue)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + -((250 - maxDataValue/(maxRange/300))+ margin.top) + ")");
 
     svgContainer
         .append("div")
         .attr("class", "svg-title")
         .style("width", '300px')
-        .text(key);
+        .text(function() {
+            if(key.length <= 40) {
+                return key
+            } else {
+                return key.substring(0,40) + " ..."
+            }
 
+        })
+        .attr("data-tippy-content", key);
+
+    tippy('.svg-title', {
+        content: '',
+        arrow: false
+
+    });
 
     x.domain([new Date(2001, 0, 1), new Date(2018, 11, 31)]);
     ySelf.domain([0, d3.max(testData, function (d) { return Math.max(d.Exported, d.Imported);  })]);
@@ -222,13 +275,13 @@ var drawMultiples = function(data, key, country, type, maxRange, container) {
         .attr("d", valueline);
 
     //Персональна шкала
-    ghost.append("path")
-        .data([testData])
-        .attr("class", "self-line")
-        .style("stroke", "grey")
-        .style("stroke-dasharray", ("3, 3"))
-        .style("fill", "none")
-        .attr("d", selfline);
+    // ghost.append("path")
+    //     .data([testData])
+    //     .attr("class", "self-line")
+    //     .style("stroke", "grey")
+    //     .style("stroke-dasharray", ("3, 3"))
+    //     .style("fill", "none")
+    //     .attr("d", selfline);
 
 
     ghost
@@ -242,11 +295,11 @@ var drawMultiples = function(data, key, country, type, maxRange, container) {
                 return x(d.year) - 15;
             })
         .attr("y", function(d) {
-                return ySelf(d[type]) - 10;
+                return y(d[type]) - 10;
 
         })
         .style("font-size", '10px')
-        .style("fill", 'grey')
+        .style("fill", 'black')
         .text(function(d){
             return Math.floor(d[type]/1000);
         })
@@ -256,6 +309,13 @@ var drawMultiples = function(data, key, country, type, maxRange, container) {
     // Add the X Axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x).tickSize(-height));
+
+    svg.selectAll(".tick > line")
+        .attr("stroke", "lightgrey")
+
+
 };
+
+
 
